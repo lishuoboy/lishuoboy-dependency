8ec5aed5840c519d084fd01afd5b92a915f6c67e
lishuoboy <25135394@qq.com>
2022-09-04 21:03:29 +0800

    MySbUtil,增加参数：是否打开url

--- lishuoboy-dependency-sb/src/main/java/top/lishuoboy/dependency/sb/sb/MySbUtil.java
+++ lishuoboy-dependency-sb/src/main/java/top/lishuoboy/dependency/sb/sb/MySbUtil.java
@@ -3,6 +3,8 @@ package top.lishuoboy.dependency.sb.sb;
 import cn.hutool.core.date.BetweenFormatter;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.net.NetUtil;
+import cn.hutool.core.util.RuntimeUtil;
+import cn.hutool.core.util.StrUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
@@ -17,7 +19,7 @@ import org.springframework.context.ApplicationContext;
 public class MySbUtil {
 
     /** 启动 SB 并 输出 “访问地址”、“启动用时” */
-    public static void run(Class clazz, String[] args) {
+    public static void run(Class clazz, String[] args, boolean openUrl) {
         long startDate = DateUtil.current();
 
         ApplicationContext context = SpringApplication.run(clazz, args);
@@ -26,13 +28,24 @@ public class MySbUtil {
         String ip = NetUtil.getLocalhostStr();
         String port = context.getEnvironment().getProperty("server.port", "8080");
         String path = context.getEnvironment().getProperty("server.servlet.context-path", "/");
+        String remoteUrl = StrUtil.format("http://{}:{}{}", ip, port, path);
+        String localUrl = StrUtil.format("http://{}:{}{}", "localhost", port, path);
 
         log.info(""
                 + "\n====================启动成功===================="
                 + "\n\t启动用时:\t {}"
-                + "\n\t远程地址:\t http://{}:{}{}"
-                + "\n\t本地地址:\t http://localhost:{}{}"
+                + "\n\t远程地址:\t {}"
+                + "\n\t本地地址:\t {}"
                 + "\n====================启动成功====================\n"
-            , duration, ip, port, path, port, path);
+            , duration, remoteUrl, localUrl);
+
+        if (openUrl) {
+            RuntimeUtil.exec("cmd /c start " + remoteUrl);  // 打开网址
+        }
+    }
+
+    /** 启动 SB 并 输出 “访问地址”、“启动用时”。不打开网址 */
+    public static void run(Class clazz, String[] args) {
+        run(clazz, args, false);
     }
 }
