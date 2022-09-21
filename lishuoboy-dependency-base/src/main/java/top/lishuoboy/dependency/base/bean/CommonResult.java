7766625b942265cba1a1b4bfac647ed854ea487a
lishuoboy <lishuoboy@qq.com>
2022-09-21 17:57:55 +0800

    User.java 的@Builder改为 @SuperBuilder + @Accessors(chain = true)

--- lishuoboy-dependency-base/src/main/java/top/lishuoboy/dependency/base/bean/CommonResult.java
+++ lishuoboy-dependency-base/src/main/java/top/lishuoboy/dependency/base/bean/CommonResult.java
@@ -1,14 +1,16 @@
 package top.lishuoboy.dependency.base.bean;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import lombok.experimental.SuperBuilder;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@SuperBuilder
+@Accessors(chain = true)
 public class CommonResult<T> {
     private Integer code;
     private String msg;
